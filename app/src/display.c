renoce/scrcpy-source

commit 02989249f6a992090a70c4d61e0834a990f004fa

Migrate from SDL2 to SDL3
Refs <https://wiki.libsdl.org/SDL3/README-migration>
PR #6216 <https://github.com/Genymobile/scrcpy/pull/6216>

app/src/display.c
@@ -6,14 +6,18 @@
 #include <libavutil/pixfmt.h>
 
 #include "util/log.h"
+#include "util/sdl.h"
 
 static bool
 sc_display_init_novideo_icon(struct sc_display *display,
                              SDL_Surface *icon_novideo) {
     assert(icon_novideo);
 
-    if (SDL_RenderSetLogicalSize(display->renderer,
-                                 icon_novideo->w, icon_novideo->h)) {
+    bool ok = SDL_SetRenderLogicalPresentation(display->renderer,
+                                               icon_novideo->w,
+                                               icon_novideo->h,
+                                        SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    if (!ok) {
         LOGW("Could not set renderer logical size: %s", SDL_GetError());
         // don't fail
     }
@@ -31,16 +35,13 @@ sc_display_init_novideo_icon(struct sc_display *display,
 bool
 sc_display_init(struct sc_display *display, SDL_Window *window,
                 SDL_Surface *icon_novideo, bool mipmaps) {
-    display->renderer =
-        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    display->renderer = SDL_CreateRenderer(window, NULL);
     if (!display->renderer) {
         LOGE("Could not create renderer: %s", SDL_GetError());
         return false;
     }
 
-    SDL_RendererInfo renderer_info;
-    int r = SDL_GetRendererInfo(display->renderer, &renderer_info);
-    const char *renderer_name = r ? NULL : renderer_info.name;
+    const char *renderer_name = SDL_GetRendererName(display->renderer);
     LOGI("Renderer: %s", renderer_name ? renderer_name : "(unknown)");
 
     display->mipmaps = false;
@@ -56,8 +57,11 @@ sc_display_init(struct sc_display *display, SDL_Window *window,
 #ifdef SC_DISPLAY_FORCE_OPENGL_CORE_PROFILE
         // Persuade macOS to give us something better than OpenGL 2.1.
         // If we create a Core Profile context, we get the best OpenGL version.
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
-                            SDL_GL_CONTEXT_PROFILE_CORE);
+        bool ok = SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                                      SDL_GL_CONTEXT_PROFILE_CORE);
+        if (!ok) {
+            LOGW("Could not set a GL Core Profile Context");
+        }
 
         LOGD("Creating OpenGL Core Profile context");
         display->gl_context = SDL_GL_CreateContext(window);
@@ -100,7 +104,7 @@ sc_display_init(struct sc_display *display, SDL_Window *window,
         bool ok = sc_display_init_novideo_icon(display, icon_novideo);
         if (!ok) {
 #ifdef SC_DISPLAY_FORCE_OPENGL_CORE_PROFILE
-            SDL_GL_DeleteContext(display->gl_context);
+            SDL_GL_DestroyContext(display->gl_context);
 #endif
             SDL_DestroyRenderer(display->renderer);
             return false;
@@ -116,7 +120,7 @@ sc_display_destroy(struct sc_display *display) {
         av_frame_free(&display->pending.frame);
     }
 #ifdef SC_DISPLAY_FORCE_OPENGL_CORE_PROFILE
-    SDL_GL_DeleteContext(display->gl_context);
+    SDL_GL_DestroyContext(display->gl_context);
 #endif
     if (display->texture) {
         SDL_DestroyTexture(display->texture);
@@ -124,20 +128,25 @@ sc_display_destroy(struct sc_display *display) {
     SDL_DestroyRenderer(display->renderer);
 }
 
-static SDL_YUV_CONVERSION_MODE
-sc_display_to_sdl_color_mode(enum AVColorSpace color_space,
-                             enum AVColorRange color_range) {
+static enum SDL_Colorspace
+sc_display_to_sdl_color_space(enum AVColorSpace color_space,
+                              enum AVColorRange color_range) {
+    bool full_range = color_range == AVCOL_RANGE_JPEG;
+
     switch (color_space) {
         case AVCOL_SPC_BT709:
-            return SDL_YUV_CONVERSION_BT709;
+            return full_range ? SDL_COLORSPACE_BT709_FULL
+                              : SDL_COLORSPACE_BT709_LIMITED;
         case AVCOL_SPC_BT470BG:
         case AVCOL_SPC_SMPTE170M:
-            return SDL_YUV_CONVERSION_BT601;
+            return full_range ? SDL_COLORSPACE_BT601_FULL
+                              : SDL_COLORSPACE_BT601_LIMITED;
+        case AVCOL_SPC_BT2020_NCL:
+        case AVCOL_SPC_BT2020_CL:
+            return full_range ? SDL_COLORSPACE_BT2020_FULL
+                              : SDL_COLORSPACE_BT2020_LIMITED;
         default:
-            if (color_range == AVCOL_RANGE_JPEG) {
-                return SDL_YUV_CONVERSION_JPEG;
-            }
-            return SDL_YUV_CONVERSION_AUTOMATIC;
+            return SDL_COLORSPACE_JPEG;
     }
 }
 
@@ -145,10 +154,36 @@ static SDL_Texture *
 sc_display_create_texture(struct sc_display *display,
                           struct sc_size size, enum AVColorSpace color_space,
                           enum AVColorRange color_range) {
+    SDL_PropertiesID props = SDL_CreateProperties();
+    if (!props) {
+        return NULL;
+    }
+
+    enum SDL_Colorspace sdl_color_space =
+        sc_display_to_sdl_color_space(color_space, color_range);
+
+    bool ok =
+        SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_FORMAT_NUMBER,
+                              SDL_PIXELFORMAT_YV12);
+    ok &= SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_ACCESS_NUMBER,
+                                SDL_TEXTUREACCESS_STREAMING);
+    ok &= SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_WIDTH_NUMBER,
+                                size.width);
+    ok &= SDL_SetNumberProperty(props, SDL_PROP_TEXTURE_CREATE_HEIGHT_NUMBER,
+                                size.height);
+    ok &= SDL_SetNumberProperty(props,
+                                SDL_PROP_TEXTURE_CREATE_COLORSPACE_NUMBER,
+                                sdl_color_space);
+
+    if (!ok) {
+        LOGE("Could not set texture properties");
+        SDL_DestroyProperties(props);
+        return NULL;
+    }
+
     SDL_Renderer *renderer = display->renderer;
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_YV12,
-                                             SDL_TEXTUREACCESS_STREAMING,
-                                             size.width, size.height);
+    SDL_Texture *texture = SDL_CreateTextureWithProperties(renderer, props);
+    SDL_DestroyProperties(props);
     if (!texture) {
         LOGD("Could not create texture: %s", SDL_GetError());
         return NULL;
@@ -157,20 +192,37 @@ sc_display_create_texture(struct sc_display *display,
     if (display->mipmaps) {
         struct sc_opengl *gl = &display->gl;
 
-        SDL_GL_BindTexture(texture, NULL, NULL);
+        SDL_PropertiesID props = SDL_GetTextureProperties(texture);
+        if (!props) {
+            LOGE("Could not get texture properties: %s", SDL_GetError());
+            SDL_DestroyTexture(texture);
+            return NULL;
+        }
+
+        const char *renderer_name = SDL_GetRendererName(display->renderer);
+        const char *key = !renderer_name || !strcmp(renderer_name, "opengl")
+                        ? SDL_PROP_TEXTURE_OPENGL_TEXTURE_NUMBER
+                        : SDL_PROP_TEXTURE_OPENGLES2_TEXTURE_NUMBER;
+
+        int64_t texture_id = SDL_GetNumberProperty(props, key, 0);
+        SDL_DestroyProperties(props);
+        if (!texture_id) {
+            LOGE("Could not get texture id: %s", SDL_GetError());
+            SDL_DestroyTexture(texture);
+            return NULL;
+        }
+
+        assert(!(texture_id & ~0xFFFFFFFF)); // fits in uint32_t
+        display->texture_id = texture_id;
+        gl->BindTexture(GL_TEXTURE_2D, display->texture_id);
 
         // Enable trilinear filtering for downscaling
         gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                           GL_LINEAR_MIPMAP_LINEAR);
         gl->TexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -1.f);
 
-        SDL_GL_UnbindTexture(texture);
+        gl->BindTexture(GL_TEXTURE_2D, 0);
     }
-
-    // Configure YUV color range conversion
-    SDL_YUV_CONVERSION_MODE sdl_color_mode =
-        sc_display_to_sdl_color_mode(color_space, color_range);
-    SDL_SetYUVConversionMode(sdl_color_mode);
 
     return texture;
 }
@@ -282,19 +334,22 @@ sc_display_prepare_texture(struct sc_display *display, struct sc_size size,
 static bool
 sc_display_update_texture_internal(struct sc_display *display,
                                    const AVFrame *frame) {
-    int ret = SDL_UpdateYUVTexture(display->texture, NULL,
+    bool ok = SDL_UpdateYUVTexture(display->texture, NULL,
                                    frame->data[0], frame->linesize[0],
                                    frame->data[1], frame->linesize[1],
                                    frame->data[2], frame->linesize[2]);
-    if (ret) {
+    if (!ok) {
         LOGD("Could not update texture: %s", SDL_GetError());
         return false;
     }
 
     if (display->mipmaps) {
-        SDL_GL_BindTexture(display->texture, NULL, NULL);
-        display->gl.GenerateMipmap(GL_TEXTURE_2D);
-        SDL_GL_UnbindTexture(display->texture);
+        assert(display->texture_id);
+        struct sc_opengl *gl = &display->gl;
+
+        gl->BindTexture(GL_TEXTURE_2D, display->texture_id);
+        gl->GenerateMipmap(GL_TEXTURE_2D);
+        gl->BindTexture(GL_TEXTURE_2D, 0);
     }
 
     return true;
@@ -319,7 +374,7 @@ sc_display_update_texture(struct sc_display *display, const AVFrame *frame) {
 enum sc_display_result
 sc_display_render(struct sc_display *display, const SDL_Rect *geometry,
                   enum sc_orientation orientation) {
-    SDL_RenderClear(display->renderer);
+    sc_sdl_render_clear(display->renderer);
 
     if (display->pending.flags) {
         bool ok = sc_display_apply_pending(display);
@@ -332,8 +387,10 @@ sc_display_render(struct sc_display *display, const SDL_Rect *geometry,
     SDL_Texture *texture = display->texture;
 
     if (orientation == SC_ORIENTATION_0) {
-        int ret = SDL_RenderCopy(renderer, texture, NULL, geometry);
-        if (ret) {
+        SDL_FRect frect;
+        SDL_RectToFRect(geometry, &frect);
+        bool ok = SDL_RenderTexture(renderer, texture, NULL, &frect);
+        if (!ok) {
             LOGE("Could not render texture: %s", SDL_GetError());
             return SC_DISPLAY_RESULT_ERROR;
         }
@@ -341,29 +398,27 @@ sc_display_render(struct sc_display *display, const SDL_Rect *geometry,
         unsigned cw_rotation = sc_orientation_get_rotation(orientation);
         double angle = 90 * cw_rotation;
 
-        const SDL_Rect *dstrect = NULL;
-        SDL_Rect rect;
+        SDL_FRect frect;
         if (sc_orientation_is_swap(orientation)) {
-            rect.x = geometry->x + (geometry->w - geometry->h) / 2;
-            rect.y = geometry->y + (geometry->h - geometry->w) / 2;
-            rect.w = geometry->h;
-            rect.h = geometry->w;
-            dstrect = &rect;
+            frect.x = geometry->x + (geometry->w - geometry->h) / 2.f;
+            frect.y = geometry->y + (geometry->h - geometry->w) / 2.f;
+            frect.w = geometry->h;
+            frect.h = geometry->w;
         } else {
-            dstrect = geometry;
+            SDL_RectToFRect(geometry, &frect);
         }
 
-        SDL_RendererFlip flip = sc_orientation_is_mirror(orientation)
+        SDL_FlipMode flip = sc_orientation_is_mirror(orientation)
                               ? SDL_FLIP_HORIZONTAL : 0;
 
-        int ret = SDL_RenderCopyEx(renderer, texture, NULL, dstrect, angle,
-                                   NULL, flip);
-        if (ret) {
+        bool ok = SDL_RenderTextureRotated(renderer, texture, NULL, &frect,
+                                           angle, NULL, flip);
+        if (!ok) {
             LOGE("Could not render texture: %s", SDL_GetError());
             return SC_DISPLAY_RESULT_ERROR;
         }
     }
 
-    SDL_RenderPresent(display->renderer);
+    sc_sdl_render_present(display->renderer);
     return SC_DISPLAY_RESULT_OK;
 }

app/meson.build
@@ -57,6 +57,7 @@ src = [
     'src/util/process.c',
     'src/util/process_intr.c',
     'src/util/rand.c',
+    'src/util/sdl.c',
     'src/util/strbuf.c',
     'src/util/str.c',
     'src/util/term.c',
@@ -117,7 +118,7 @@ dependencies = [
     dependency('libavcodec', version: '>= 57.37', static: static),
     dependency('libavutil', static: static),
     dependency('libswresample', static: static),
-    dependency('sdl2', version: '>= 2.0.5', static: static),
+    dependency('sdl3', version: '>= 3.2.0', static: static),
 ]
 
 if v4l2_support
@@ -275,7 +276,7 @@ if get_option('buildtype') == 'debug'
         exe = executable(t[0], sources,
                          include_directories: src_dir,
                          dependencies: dependencies,
-                         c_args: ['-DSDL_MAIN_HANDLED', '-DSC_TEST'])
+                         c_args: ['-DSC_TEST'])
         test(t[0], exe)
     endforeach
 endif

app/src/audio_player.c
@@ -1,23 +1,40 @@
 #include "audio_player.h"
 
 #include "util/log.h"
+#include "SDL3/SDL_hints.h"
 
 /** Downcast frame_sink to sc_audio_player */
 #define DOWNCAST(SINK) container_of(SINK, struct sc_audio_player, frame_sink)
 
-#define SC_SDL_SAMPLE_FMT AUDIO_F32
+#define SC_SDL_SAMPLE_FMT SDL_AUDIO_F32LE
 
 static void SDLCALL
-sc_audio_player_sdl_callback(void *userdata, uint8_t *stream, int len_int) {
+sc_audio_player_stream_callback(void *userdata, SDL_AudioStream *stream,
+                                int additional_amount, int total_amount) {
+    (void) total_amount;
+
     struct sc_audio_player *ap = userdata;
 
-    assert(len_int > 0);
-    size_t len = len_int;
-
+    size_t len = additional_amount;
     assert(len % ap->audioreg.sample_size == 0);
-    uint32_t out_samples = len / ap->audioreg.sample_size;
 
-    sc_audio_regulator_pull(&ap->audioreg, stream, out_samples);
+    // The requested amount may exceed the internal aout_buffer size.
+    // In this (unlikely) case, send the data to the stream in multiple chunks.
+    while (len) {
+        size_t chunk_size = MIN(ap->aout_buffer_size, len);
+        uint32_t out_samples = chunk_size / ap->audioreg.sample_size;
+        sc_audio_regulator_pull(&ap->audioreg, ap->aout_buffer,
+                                out_samples);
+
+        assert(chunk_size <= len);
+        len -= chunk_size;
+        bool ok =
+            SDL_PutAudioStreamData(stream, ap->aout_buffer, chunk_size);
+        if (!ok) {
+            LOGW("Audio stream error: %s", SDL_GetError());
+            return;
+        }
+    }
 }
 
 static bool
@@ -61,22 +78,44 @@ sc_audio_player_frame_sink_open(struct sc_frame_sink *sink,
                                                        / SC_TICK_FREQ;
     assert(aout_samples <= 0xFFFF);
 
-    SDL_AudioSpec desired = {
-        .freq = ctx->sample_rate,
-        .format = SC_SDL_SAMPLE_FMT,
-        .channels = nb_channels,
-        .samples = aout_samples,
-        .callback = sc_audio_player_sdl_callback,
-        .userdata = ap,
-    };
-    SDL_AudioSpec obtained;
-
-    ap->device = SDL_OpenAudioDevice(NULL, 0, &desired, &obtained, 0);
-    if (!ap->device) {
-        LOGE("Could not open audio device: %s", SDL_GetError());
+    char str[5 + 1]; // max 65535
+    int r = snprintf(str, sizeof(str), "%" PRIu16, (uint16_t) aout_samples);
+    assert(r >= 0 && (size_t) r < sizeof(str));
+    (void) r;
+    if (!SDL_SetHint(SDL_HINT_AUDIO_DEVICE_SAMPLE_FRAMES, str)) {
+        LOGE("Could not set audio output buffer");
         sc_audio_regulator_destroy(&ap->audioreg);
         return false;
     }
+
+    // Make the buffer at least 1024 samples long (the hint is not always
+    // honored)
+    uint64_t aout_buffer_samples = MAX(1024, aout_samples);
+    ap->aout_buffer_size = aout_buffer_samples * sample_size;
+    ap->aout_buffer = malloc(ap->aout_buffer_size);
+    if (!ap->aout_buffer) {
+        sc_audio_regulator_destroy(&ap->audioreg);
+        return false;
+    }
+
+    SDL_AudioSpec spec = {
+        .freq = ctx->sample_rate,
+        .format = SC_SDL_SAMPLE_FMT,
+        .channels = nb_channels,
+    };
+
+    ap->stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK,
+                                           &spec,
+                                           sc_audio_player_stream_callback, ap);
+    if (!ap->stream) {
+        LOGE("Could not open audio device: %s", SDL_GetError());
+        free(ap->aout_buffer);
+        sc_audio_regulator_destroy(&ap->audioreg);
+        return false;
+    }
+
+    ap->device = SDL_GetAudioStreamDevice(ap->stream);
+    assert(ap->device);
 
     // The thread calling open() is the thread calling push(), which fills the
     // audio buffer consumed by the SDL audio thread.
@@ -86,7 +125,14 @@ sc_audio_player_frame_sink_open(struct sc_frame_sink *sink,
         (void) ok; // We don't care if it worked, at least we tried
     }
 
-    SDL_PauseAudioDevice(ap->device, 0);
+    ok = SDL_ResumeAudioDevice(ap->device);
+    if (!ok) {
+        LOGE("Could not resume audio device: %s", SDL_GetError());
+        SDL_DestroyAudioStream(ap->stream);
+        free(ap->aout_buffer);
+        sc_audio_regulator_destroy(&ap->audioreg);
+        return false;
+    }
 
     return true;
 }
@@ -95,11 +141,16 @@ static void
 sc_audio_player_frame_sink_close(struct sc_frame_sink *sink) {
     struct sc_audio_player *ap = DOWNCAST(sink);
 
+    assert(ap->stream);
     assert(ap->device);
-    SDL_PauseAudioDevice(ap->device, 1);
-    SDL_CloseAudioDevice(ap->device);
+    SDL_PauseAudioDevice(ap->device);
+
+    // ap->device is owned by ap->stream
+    SDL_DestroyAudioStream(ap->stream);
 
     sc_audio_regulator_destroy(&ap->audioreg);
+
+    free(ap->aout_buffer);
 }
 
 void

app/src/audio_player.h
@@ -3,7 +3,7 @@
 
 #include "common.h"
 
-#include <SDL2/SDL_audio.h>
+#include <SDL3/SDL_audio.h>
 
 #include "audio_regulator.h"
 #include "trait/frame_sink.h"
@@ -22,7 +22,11 @@ struct sc_audio_player {
     // SDL audio output buffer size
     sc_tick output_buffer_duration;
 
-    SDL_AudioDeviceID device;
+    uint8_t *aout_buffer;
+    size_t aout_buffer_size;
+
+    SDL_AudioStream *stream;
+    SDL_AudioDeviceID device; // owned by the audio stream
     struct sc_audio_regulator audioreg;
 };
 

app/src/compat.h
@@ -6,7 +6,7 @@
 #include <libavcodec/version.h>
 #include <libavformat/version.h>
 #include <libavutil/version.h>
-#include <SDL2/SDL_version.h>
+#include <SDL3/SDL_version.h>
 
 #ifndef _WIN32
 # define PRIu64_ PRIu64
@@ -59,28 +59,6 @@
 //   from AVFormatContext.codecpar should be used from now on.
 #if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(60, 15, 100)
 # define SCRCPY_LAVC_HAS_CODECPAR_CODEC_SIDEDATA
-#endif
-
-#if SDL_VERSION_ATLEAST(2, 0, 6)
-// <https://github.com/libsdl-org/SDL/commit/d7a318de563125e5bb465b1000d6bc9576fbc6fc>
-# define SCRCPY_SDL_HAS_HINT_TOUCH_MOUSE_EVENTS
-#endif
-
-#if SDL_VERSION_ATLEAST(2, 0, 8)
-// <https://hg.libsdl.org/SDL/rev/dfde5d3f9781>
-# define SCRCPY_SDL_HAS_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR
-#endif
-
-#if SDL_VERSION_ATLEAST(2, 0, 16)
-# define SCRCPY_SDL_HAS_THREAD_PRIORITY_TIME_CRITICAL
-#endif
-
-#if SDL_VERSION_ATLEAST(2, 0, 18)
-# define SCRCPY_SDL_HAS_HINT_APP_NAME
-#endif
-
-#if SDL_VERSION_ATLEAST(2, 0, 14)
-# define SCRCPY_SDL_HAS_HINT_AUDIO_DEVICE_APP_NAME
 #endif
 
 #ifndef HAVE_STRDUP

app/src/display.h
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <libavutil/frame.h>
-#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
 
 #include "coords.h"
 #include "opengl.h"
@@ -26,6 +26,7 @@ struct sc_display {
 #endif
 
     bool mipmaps;
+    uint32_t texture_id; // only set if mipmaps is enabled
 
     struct {
 #define SC_DISPLAY_PENDING_FLAG_TEXTURE 1

app/src/events.c
@@ -9,11 +9,8 @@ bool
 sc_push_event_impl(uint32_t type, const char *name) {
     SDL_Event event;
     event.type = type;
-    int ret = SDL_PushEvent(&event);
-    // ret < 0: error (queue full)
-    // ret == 0: event was filtered
-    // ret == 1: success
-    if (ret != 1) {
+    bool ok = SDL_PushEvent(&event);
+    if (!ok) {
         LOGE("Could not post %s event: %s", name, SDL_GetError());
         return false;
     }
@@ -30,34 +27,25 @@ sc_post_to_main_thread(sc_runnable_fn run, void *userdata) {
             .data2 = userdata,
         },
     };
-    int ret = SDL_PushEvent(&event);
-    // ret < 0: error (queue full)
-    // ret == 0: event was filtered
-    // ret == 1: success
-    if (ret != 1) {
-        if (ret == 0) {
-            // if ret == 0, this is expected on exit, log in debug mode
-            LOGD("Could not post runnable to main thread (filtered)");
-        } else {
-            assert(ret < 0);
-            LOGW("Could not post runnable to main thread: %s", SDL_GetError());
-        }
+    bool ok = SDL_PushEvent(&event);
+    if (!ok) {
+        LOGW("Could not post runnable to main thread: %s", SDL_GetError());
         return false;
     }
 
     return true;
 }
 
-static int SDLCALL
+static bool SDLCALL
 task_event_filter(void *userdata, SDL_Event *event) {
     (void) userdata;
 
     if (event->type == SC_EVENT_RUN_ON_MAIN_THREAD) {
         // Reject this event type from now on
-        return 0;
+        return false;
     }
 
-    return 1;
+    return true;
 }
 
 void

app/src/events.h
@@ -5,10 +5,10 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <SDL2/SDL_events.h>
+#include <SDL3/SDL_events.h>
 
 enum {
-    SC_EVENT_NEW_FRAME = SDL_USEREVENT,
+    SC_EVENT_NEW_FRAME = SDL_EVENT_USER,
     SC_EVENT_RUN_ON_MAIN_THREAD,
     SC_EVENT_DEVICE_DISCONNECTED,
     SC_EVENT_SERVER_CONNECTION_FAILED,

commit fde02a7dfa3bcf7c583b7399e5d4b6c9d83baebd

Fix segfault with --no-video
Do not call SDL_RectToFRect() if geometry is NULL.

Bug introduced by 02989249f6a992090a70c4d61e0834a990f004fa.

app/src/display.c
@@ -389,8 +389,12 @@ sc_display_render(struct sc_display *display, const SDL_Rect *geometry,
 
     if (orientation == SC_ORIENTATION_0) {
         SDL_FRect frect;
-        SDL_RectToFRect(geometry, &frect);
-        bool ok = SDL_RenderTexture(renderer, texture, NULL, &frect);
+        SDL_FRect *fgeometry = NULL;
+        if (geometry) {
+            SDL_RectToFRect(geometry, &frect);
+            fgeometry = &frect;
+        }
+        bool ok = SDL_RenderTexture(renderer, texture, NULL, fgeometry);
         if (!ok) {
             LOGE("Could not render texture: %s", SDL_GetError());
             return SC_DISPLAY_RESULT_ERROR;